0a4462e9382cc865393382592cbd839679d1fe61
hanmiton <hanmilton_12@outlook.com>
2017-11-22 13:36:26 -0500

    datos base de datos2 gi

--- bower_components/js/controllers.js
+++ bower_components/js/controllers.js
@@ -45,10 +45,7 @@
       }
       $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [ingeniero.var1, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+  $scope.data =  $scope.ingenieros
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };

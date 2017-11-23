95725926cbe180c6d9ac341e6ecc72e0311728a9
hanmiton <hanmilton_12@outlook.com>
2017-11-22 23:17:34 -0500

    añadiendo a contraolador unitario

--- bower_components/js/controllers.js
+++ bower_components/js/controllers.js
@@ -43,36 +43,12 @@
           return Math.floor(index / n);
         }).toArray().value();
       }
-      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+     
 
-  ingenieros.filter(function(ingeniero){
-    console.log()
-  })
-  $scope.data =  $scope.ingenieros
-  console.log(ingenieros.name)
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
-        }
-      ]
-    }
-  };
+ 
+ 
+
+
 
     }])
     .controller('IngenieroController', ['$rootScope', '$scope', '$routeParams', 'Ingeniero', function ($rootScope, $scope, $routeParams, Ingeniero) {
@@ -80,8 +56,37 @@
        //console.log(id)
       //$scope.ingeniero = {};
         Ingeniero.get({ id: id }, function (ingeniero) {
+
+           $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+            $scope.series = ['Series A', 'Series B'];
+            $scope.data = [
+              [65, 59, 80, 81, 56, 55, 40],
+              [28, 48, 40, 19, 86, 27, 90]
+            ];
            
-           $rootScope.title = ingeniero.id;
+                         $scope.onClick = function (points, evt) {
+                console.log(points, evt);
+              };
+              $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+              $scope.options = {
+                scales: {
+                  yAxes: [
+                    {
+                      id: 'y-axis-1',
+                      type: 'linear',
+                      display: true,
+                      position: 'left'
+                    },
+                    {
+                      id: 'y-axis-2',
+                      type: 'linear',
+                      display: true,
+                      position: 'right'
+                    }
+                  ]
+                }
+              };
+           $rootScope.title = ingeniero._id;
           $scope.ingeniero = ingeniero;
           console.log(ingeniero._id)
       

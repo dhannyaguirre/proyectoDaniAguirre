85564689acf378e9db99bb70f9b16fb8eb716c35
hanmiton <hanmilton_12@outlook.com>
2017-11-22 23:29:31 -0500

    añadiendo datos desde base

--- bower_components/js/controllers.js
+++ bower_components/js/controllers.js
@@ -60,7 +60,7 @@
            $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
             $scope.series = ['Series A', 'Series B'];
             $scope.data = [
-              [65, 59, 80, 81, 56, 55, 40],
+              [ingeniero.var1, ingeniero.var2, ingeniero.var3, ingeniero.var4, ingeniero.var5, ingeniero.var6, ingeniero.var7, ingeniero.var8, ingeniero.var9, ingeniero.var10],
               [28, 48, 40, 19, 86, 27, 90]
             ];
            

bd28e7fdaaf4cc60e05d2d79cbbe58bd41c51e6a
hanmiton <hanmilton_12@outlook.com>
2017-11-25 10:38:20 -0500

    datos

--- bower_components/js/controllers.js
+++ bower_components/js/controllers.js
@@ -28,7 +28,7 @@ var i = 0
           $scope.labels = ["Medicion1", "Medicion2", "Medicion3", "Medicion4", "Medicion5", "Medicion6", "Medicion7","Medicion8","Medicion9","Medicion10"];
             $scope.series = ['Temperatura'];
             $scope.data = [
-              [data[i].var1, data[i].var2, data[i].var3, data[i].var4, data[i].var5, data[i].var6, data[i].var7, data[i].var8, data[i].var9, data[i].var10]
+              [data[i+1].var1, data[i].var2, data[i].var3, data[i].var4, data[i].var5, data[i].var6, data[i].var7, data[i].var8, data[i].var9, data[i].var10]
             ];
             console.log(data[i].var1)
               $scope.onClick = function (points, evt) {

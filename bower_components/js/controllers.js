4177792d59d68bb386da3d8939c8f6696bfb439b
hanmiton <hanmilton_12@outlook.com>
2017-11-27 02:30:47 -0500

    mandando un nsolo color2

--- bower_components/js/controllers.js
+++ bower_components/js/controllers.js
@@ -174,12 +174,6 @@ var n = 0
               if(3 > datos[n-j].var1 || limites[0].lim2 < datos[n-j].var1 ){
                 console.log('aqui estoy')
                 c1 = '#ff6384'
-                c2 = '#ff6384'
-                c3 = '#ff6384'
-                c4 = '#ff6384'
-                c5 = '#ff6384'
-                c6 = '#ff6384'
-                
               }
             }
             } else if (id == 2){
